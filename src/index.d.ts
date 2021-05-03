9e2bd2febfd9f0cdbab752652ed51fb4c418a5a3
LiNKS <a824759414@gmail.com>
2021-05-03 12:08:57 +0800

    fix: 修正ts定义声明

--- src/index.d.ts
+++ src/index.d.ts
@@ -8,19 +8,52 @@ declare module 'pika9' {
     private _throttleHold: Function | null;
     private _enable: boolean;
     private _holding: boolean;
+
+    private _init(options: Pika9.Options): void;
+    private _load(): void;
+    private _checkLoad(): void;
+    private _updateSelection(startPoint: Pika9.Point, endPoint: Pika9.Point): void;
+    private _onHoldStart(ev: Pika9.HolderEvent): void;
+    private _onHoldMove(ev: Pika9.HolderEvent): void;
+    private _onHoldEnd(ev: Pika9.HolderEvent): void;
+    public enable(): void;
+    public disable(): void;
+    public unload(): void;
+    public setSelectable(children: Pika9.Selector | Pika9.Selector[]): void;
+
+    public constructor(options: Pika9.Options);
   }
 
   namespace Pika9 {
+    type Selector = string | Node | HTMLElement;
+    type Point = { x: number, y: number, [key: string]: any }
+  
     interface Options {
-      parent: string | Node | HTMLElement;
-      children: string | Node | HTMLElement;
+      parent: Selector;
+      children: Selector | Selector[];
       threshold?: number;
-      onStart?: Function;
-      onHold?: Function;
-      onEnd?: Function;
+      onStart?: (ev: Event) => any;
+      onHold?: (ev: Event) => any;
+      onEnd?: (ev: Event) => any;
       mode?: 'toggle' | 'append' | 'disposable';
       cleanOnClick?: boolean;
     }
+
+    interface Event {
+      start: Point;
+      active: Point;
+      added: HTMLElement[];
+      removed: HTMLElement[];
+      selected: HTMLElement[];
+    }
+
+    interface HolderEvent {
+      activePoint?: Point;
+      startPoint: Point;
+      target: HTMLElement;
+      currentTarget: HTMLElement;
+    }
+  
     class Selection {}
     class Holder {}
     class ElManager {}
